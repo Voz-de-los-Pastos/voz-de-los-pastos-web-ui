21128881aec7c4767514552f625e316511595193
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-27 20:57:12 -0500

    :sparkles: settings project in to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
+import { SyntonizeModule } from './syntonize/syntonize.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    SyntonizeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
