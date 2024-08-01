1ad5ac5a813946e3742a736c62bf0ddfc40a6685
jjmuesesq <jjmuesesq@unal.edu.co>
2024-07-31 20:02:38 -0500

    :sparkles: settings page services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './syntonize/pages/home/home.component';
 import { AboutPageComponent } from './syntonize/pages/about-page/about-page.component';
+import { ServicePageComponent } from './syntonize/pages/service-page/service-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutPageComponent
+  },
+  {
+    path: 'service',
+    component: ServicePageComponent
   },
   {
     path: '**',
